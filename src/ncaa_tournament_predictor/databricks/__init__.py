c25539988fd03143a0b92d554620db3fb7504320
Cory Huebner <chuebner86@gmail.com>
2025-03-25 15:16:12 -0500

    write cleaned kaggle stats

--- src/ncaa_tournament_predictor/databricks/__init__.py
+++ src/ncaa_tournament_predictor/databricks/__init__.py
@@ -1,15 +1,11 @@
 from databricks.connect import DatabricksSession
 from pyspark.sql import SparkSession
 
-from ncaa_tournament_predictor.config import Config
 
-
-def get_databricks_spark_session() -> DatabricksSession:
-    """Get a Databricks Spark session using the configured profile"""
-    profile_name = Config.databricks_profile()
-    return DatabricksSession.builder.profile(profile_name).getOrCreate()
-
-
-# A type-hint for local environments for the globally available spark variable
-# that exists in Databricks environments
-spark: SparkSession
+def get_databricks_spark_session(profile_name: str | None = None) -> SparkSession:
+    """Get a Databricks Spark session using the provided profile or the default profile if no profile given"""
+    return (
+        DatabricksSession.builder.profile(profile_name).getOrCreate()
+        if profile_name
+        else DatabricksSession.builder.getOrCreate()
+    )
--- src/ncaa_tournament_predictor/jobs/kaggle_stats/__init__.py
+++ src/ncaa_tournament_predictor/jobs/kaggle_stats/__init__.py
@@ -1,11 +1,11 @@
 from pyspark.sql import DataFrame
 
-from ncaa_tournament_predictor.databricks import spark
-from ncaa_tournament_predictor import volumes, tables, transformation
+from ncaa_tournament_predictor import volumes, tables, transformation, databricks
 
 
 def write_cleaned_kaggle_stats() -> DataFrame:
     """Convert raw Kaggle stat CSV data into a cleaned up table for easier downstream consumption"""
+    spark = databricks.get_databricks_spark_session()
     raw_kaggle_stats = (
         spark.read.format("csv")
         .options(header=True, inferSchema=True, mergeSchema=True)
