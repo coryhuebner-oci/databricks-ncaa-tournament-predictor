4000d66bd2023a911eacb86c5a3fc4105bed8d39
Cory Huebner <chuebner86@gmail.com>
2025-03-26 15:39:19 -0500

    one more field fix

--- src/ncaa_tournament_predictor/jobs/training_data/__init__.py
+++ src/ncaa_tournament_predictor/jobs/training_data/__init__.py
@@ -8,4 +8,4 @@ def get_training_dataset() -> DataFrame:
     spark = databricks.get_databricks_spark_session()
     kaggle_stats = spark.read.table(tables.cleaned_kaggle_stats)
     head_to_head_results = spark.read.table(tables.cleaned_head_to_head_results)
-    return kaggle_stats.join(head_to_head_results, on="year")
+    return kaggle_stats.join(head_to_head_results, on="college_season")
